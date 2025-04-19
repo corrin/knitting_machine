0aef511f587213fa1b1d3440acac13279c011131
Corrin Lakeland <corrin.lakeland@countdown.co.nz>
2025-04-20 10:24:05 +1200

    Migrated Python 2 to 3.  No changes to the logic of the code.

--- app/tkapp/Config.py
+++ app/tkapp/Config.py
@@ -1,12 +1,23 @@
 ﻿import os
+import platform
 
 class Config:
     def __init__(self):
-        self.imgdir = "img";
+        # platform specific configuration:
+        # TODO: load configuration from external file
+        self.imgdir = "img"
         if os.sys.platform == 'win32':
             self.device = "com34"
 #            self.datFile = 'C:/Documents and Settings/ondro/VirtualBox shared folder/knitting/knitting_machine-master/img/zaloha vzory stroj python.dat'
-            self.datFile = 'C:/Documents and Settings/ondro/VirtualBox shared folder/knitting/knitting_machine-master/file-06.dat'
+            self.datFile = '' # Use empty string for Windows default.  Used to be file-06.dat
             self.simulateEmulator = True
         else:
             self.device = "/dev/ttyUSB0"
+            self.datFile = "img/file-01.dat"
+            self.simulateEmulator = False
+        
+    def getDatFile(self):
+        return self.datFile
+    
+    def getDevice(self):
+        return self.device
